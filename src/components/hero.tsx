805e46594d968fedc8aeb3dcac47de025e786901
Ayberk Uman <ayberkuman1@gmail.com>
2024-03-26 18:37:57 +0300

    clean up

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -78,7 +78,7 @@ export default function Hero({ data }: { data: Application[] }) {
     <>
       <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="flex items-baseline justify-between border-b border-gray-200 pb-6 pt-24">
-          <h1 className="text-4xl font-bold tracking-tight text-gray-900">
+          <h1 className="sm:text-4xl font-bold tracking-tight text-gray-900">
             Applications
           </h1>
 
